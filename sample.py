be1e877ee35b60495ec3d18330a6cf3d40c3fc4a
saii340 <111050937+saii340@users.noreply.github.com>
2023-06-18 12:10:39 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,9 @@
-from time import sleep
-while True:
-    print("Hello")
-    sleep(1)
+number = 10
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
